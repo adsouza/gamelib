c79f49fe51311e36ea77ce7c8baf2b980af0cf42
Antonio D'souza <adsouza@gmail.com>
2015-05-08 20:54:24 -0400

    Fix compilation issues in deck.

--- go/cards/deck.go
+++ go/cards/deck.go
@@ -7,9 +7,8 @@ type Card struct {
 	Suit Suit
 	Rank Rank
 }
-const Joker Card{
-	Suit: 0
-	Rank: 0
+func (c *Card) IsJoker() bool {
+    return c.Suit == 0 && c.Rank == 0
 }
 // State is flexible and may be used to model the visibility of a card's face (i.e. up/down)
 // or the number of identical cards in a multi-deck game.
